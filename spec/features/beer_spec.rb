9a9617d2ba67db02e5b59f1165dc719daee4b931
Mika Laitinen <mika.m.laitinen@helsinki.fi>
2016-02-14 15:05:10 +0200

    Viikko 4 tehtävä 5

---
+++ spec/features/beer_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+include Helpers
+
+describe "Beer" do
+
+  before :each do
+    sign_in(username: "Pekka", password: "Foobar1")
+  end
+
+  it "is created if name is not empty" do
+    visit new_beer_path
+    fill_in('beer[name]', with: 'Olut')
+
+    expect {
+      click_button "Create Beer"
+    }.to change { Beer.count }.from(0).to(1)
+
+    expect(Beer.count).to eq(1)
+
+  end
+
+  it "is not created if name is empty" do
+    visit new_beer_path
+    click_button "Create Beer"
+
+    expect(current_path).to eq(beers_path)
+    expect(page).to have_content "Name can't be blank"
+    expect(Beer.count).to eq(0)
+  end
+
+end
