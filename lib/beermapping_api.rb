77858fca10eaf7e0006642689a62892b92087a35
Mika Laitinen <mika.m.laitinen@helsinki.fi>
2016-02-21 18:52:02 +0200

    viikko 5 tehtävä 4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,6 +1,13 @@
 class BeermappingApi
   def self.places_in(city)
-    #url = "http://beermapping.com/webservice/loccity/#{key}/"
+    city = city.downcase
+    Rails.cache.fetch(city) { fetch_places_in(city) }
+  end
+
+  private
+
+  def self.fetch_places_in(city)
+    # url = "http://beermapping.com/webservice/loccity/#{key}/"
     url = 'http://stark-oasis-9187.herokuapp.com/api/'
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
@@ -9,7 +16,7 @@ class BeermappingApi
     return [] if places.is_a?(Hash) and places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
-    places.map do |place|
+    places.map do | place |
       Place.new(place)
     end
   end
